c00e8047034d45ddbd5c5b01e6823ed2d489f780
saraken357 <59227062+saraken357@users.noreply.github.com>
2022-12-31 21:02:51 +0900

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.ui.Model;
+
+@Controller
+public class HelloController {
+  @GetMapping("/hello")
+  @ResponseBody
+  public String helloWorld() {
+    return "Hello,World";
+  }
+
+  // @GetMapping("/greet")
+  // @ResponseBody
+  // public String greet() {
+  //   return "こんにちは";
+  // }
+   
+  // @GetMapping("/sayHello")
+  // public String sayHello(){
+  //   return "hello";
+  // }
+
+  // @GetMapping("/greetModel")
+  // public String greetModel(Model model) {
+  //   String word = "Hello,World！（model）";
+  //   model.addAttribute("message", word);
+  //   return "greetmodel";
+  // }
+}
